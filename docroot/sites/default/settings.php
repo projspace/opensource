76099f7a106b483b5b383e313fc1c660ff596822
acquia-fields-webnode <acquia-fields-webnode@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-08-26 17:52:58 +0000

    Initialize sites/defaults/settings.php with correct db_url.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@3 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -89,7 +89,7 @@
  *   $db_url = 'mysqli://username:password@localhost/databasename';
  *   $db_url = 'pgsql://username:password@localhost/databasename';
  */
-$db_url = 'mysql://username:password@localhost/databasename';
+$db_url = 'mysql://opensource:VDa7xUVUuz3syiB@dbmaster-1/opensource';
 $db_prefix = '';
 
 /**
