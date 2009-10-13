8eca609f631c53dfea666379a55e7ca5ced5c957
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-10-13 23:52:22 +0000

    Updating the search box
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@77 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/js/script.js
+++ docroot/sites/default/themes/sitetheme/js/script.js
@@ -1,6 +1,6 @@
 (function($){
   Drupal.behaviors.sitetheme = function(context) {
-    $('#search-theme-form .form-text').focus(function() {
+    $('#search-theme-form .form-text').val(Drupal.t('Search opensource.com')).focus(function() {
       $(this).val(Drupal.t(''));
     }).blur(function() {
       $(this).val(Drupal.t('Search opensource.com'));
