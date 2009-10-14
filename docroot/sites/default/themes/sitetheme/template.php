f3bf31dc56cecf8e41c5d4bf86f60c00f59082ef
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-10-14 14:19:40 +0000

    Updating theme.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@80 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/template.php
+++ docroot/sites/default/themes/sitetheme/template.php
@@ -98,11 +98,11 @@ function STARTERKIT_preprocess(&$vars, $hook) {
  * @param $hook
  *   The name of the template being rendered ("page" in this case.)
  */
-/* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_page(&$vars, $hook) {
-  $vars['sample_variable'] = t('Lorem ipsum.');
+function sitetheme_preprocess_page(&$vars, $hook) {
+  if (!empth($vars['navbar'])) {
+    $vars['body_classes_array'][] = 'with-channel-header';
+  }
 }
-// */
 
 /**
  * Override or insert variables into the node templates.
