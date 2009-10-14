fc62a97ceb6e20bde7585c506e81f3d428997d75
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-10-14 16:10:07 +0000

    Updating the theme.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@96 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/templates/page.tpl.php
+++ docroot/sites/default/themes/sitetheme/templates/page.tpl.php
@@ -168,7 +168,7 @@
 
         <?php if ($breadcrumb || $title || $tabs || $help || $messages): ?>
           <div id="content-header">
-            <?php print $breadcrumb; ?>
+            <?php // print $breadcrumb; ?>
             <?php if ($title): ?>
               <h1 class="title"><?php print $title; ?></h1>
             <?php endif; ?>
