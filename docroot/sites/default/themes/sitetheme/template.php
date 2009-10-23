a9747469f1ae11c0c9de124e42d43d89edc5b15e
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-10-23 01:15:57 +0000

    Channel styling
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@190 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/template.php
+++ docroot/sites/default/themes/sitetheme/template.php
@@ -114,7 +114,7 @@ function sitetheme_preprocess_node(&$vars) {
       $vars['links_all'] .= '<span class="article-comment-count">'. format_plural($vars['node']->comment_count, '1 Comment', '@count Comments', array('@count' => $vars['node']->comment_count)) .'</span>';
     }
     $user_url = url('user/'. $vars['node']->uid);
-    $vars['links_all'] .= '<span class="article-author-info">'. t('Posted !date by !author', array('!date' => format_date($vars['node']->created, 'custom', 'j M Y'), '!author' => l($vars['node']->name, $user_url))) .'<a href="'. $user_url .'" class="article-author-feed"></a></span>';
+    $vars['links_all'] .= '<span class="article-author-info">'. t('Posted !date by !author', array('!date' => format_date($vars['node']->created, 'custom', 'j M Y'), '!author' => l($vars['node']->name, $user_url))) .'<a href="'. $user_url .'/feed" class="article-author-feed"></a></span>';
     $vars['links_all'] .= '<span class="article-node-link">'. l('Read more', 'node/'. $vars['node']->nid) .'</span>';
     $vars['links_all'] .= '</div>';
   }
