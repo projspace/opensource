531f13549fd20202f33b5fea50417ce4060751f4
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-10-26 19:00:00 +0000

    Updating block display.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@274 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/templates/block.tpl.php
+++ docroot/sites/default/themes/sitetheme/templates/block.tpl.php
@@ -41,7 +41,7 @@
     <h2 class="title"><?php print $block->subject; ?></h2>
   <?php endif; ?>
 
-  <div class="content">
+  <div class="content clear-block">
     <?php print $block->content; ?>
   </div>
 
