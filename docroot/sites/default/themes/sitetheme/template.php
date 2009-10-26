6ba805b0003702b7b418675064f5032a8e76d992
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-10-26 15:01:47 +0000

    Updating homepage display.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@227 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/template.php
+++ docroot/sites/default/themes/sitetheme/template.php
@@ -113,8 +113,7 @@ function sitetheme_preprocess_node(&$vars) {
     if ($vars['node']->comment_count > 0) {
       $vars['links_all'] .= '<span class="article-comment-count">'. format_plural($vars['node']->comment_count, '1 Comment', '@count Comments', array('@count' => $vars['node']->comment_count)) .'</span>';
     }
-    $user_url = url('user/'. $vars['node']->uid);
-    $vars['links_all'] .= '<span class="article-author-info">'. t('Posted !date by !author', array('!date' => format_date($vars['node']->created, 'custom', 'j M Y'), '!author' => l($vars['node']->name, 'user/'. $vars['node']->uid))) .'<a href="'. $user_url .'/feed" class="article-author-feed"></a></span>';
+    $vars['links_all'] .= '<span class="article-author-info">'. t('Posted !date by !author', array('!date' => format_date($vars['node']->created, 'custom', 'j M Y'), '!author' => l($vars['node']->name, 'user/'. $vars['node']->uid))) .'<a href="/user/'. $vars['node']->uid .'/feed" class="article-author-feed"></a></span>';
     $vars['links_all'] .= '<span class="article-node-link">'. l('Read more', 'node/'. $vars['node']->nid) .'</span>';
     $vars['links_all'] .= '</div>';
   }
@@ -130,8 +129,7 @@ function sitetheme_preprocess_views_view_fields__channel_description__block_1(&$
 }
 
 function sitetheme_preprocess_views_view_field__name(&$vars) {
-  $user_url = url('user/'. $vars['row']->users_uid);
-  $vars['output'] .= '<a href="'. $user_url .'/feed" class="article-author-feed"></a>';
+  $vars['output'] .= '<a href="/user/'. $vars['row']->users_uid .'/feed" class="article-author-feed"></a>';
 }
 
 function sitetheme_links($links, $attributes = array('class' => 'links')) {
