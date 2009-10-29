9ad1b7a9d89a2182b8f0bdde445b46c9442cfb14
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-10-29 23:19:53 +0000

    Updates for legal and openid
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@462 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/js/script.js
+++ docroot/sites/default/themes/sitetheme/js/script.js
@@ -6,7 +6,19 @@
       $(this).val(Drupal.t('Search opensource.com'));
     });
 
-    $openidLegal = $(".openid-legal-checkbox");
+    $openidLegal = $(".openid-legal-checkbox", context);
     $openidLegal.hide();
+    $("li.openid-link:not(.openid-legal-processed)", context)
+    .addClass('openid-legal-processed')
+    .click( function() {
+      $(".openid-legal-checkbox", context).hide();
+      return false;
+    });
+    $("li.user-link:not(.openid-legal-processed)", context)
+    .addClass('openid-legal-processed')
+    .click(function() {
+      $(".openid-legal-checkbox", context).show();
+      return false;
+    });
   }
 })(jQuery)
