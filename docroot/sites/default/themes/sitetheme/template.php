8af340e72d2a2037254914ef18c62f1571c01139
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-10-29 22:56:04 +0000

    Updates for legal and openid
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@457 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/template.php
+++ docroot/sites/default/themes/sitetheme/template.php
@@ -307,7 +307,7 @@ function sitetheme_preprocess_comment_wrapper(&$variables) {
 }
 
 function sitetheme_user_login($form) {
-  $form['legal_checkbox'] = array(
+  $form['openid_identifier']['legal_checkbox'] = array(
     '#type' => 'checkbox',
     '#required' => TRUE,
     '#title' => t('Accept the !terms to continue.', array('!terms' => l('Terms and Conditions', 'terms'))),
