dbe2c605340e9372f4ae2222643038e119469942
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-10-29 21:57:12 +0000

    Updates for legal and openid
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@449 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/template.php
+++ docroot/sites/default/themes/sitetheme/template.php
@@ -68,9 +68,9 @@
  */
 function sitetheme_theme(&$existing, $type, $theme, $path) {
   $hooks = zen_theme($existing, $type, $theme, $path);
-  $hooks['user_login'] = array(
-    'arguments' => array('form' => NULL),
-  );
+  //$hooks['user_login'] = array(
+  //  'arguments' => array('form' => NULL),
+  //);
   // @TODO: Needs detailed comments. Patches welcome!
   return $hooks;
 }
