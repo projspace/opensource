f7f8dbe697d5ad8dde0c33a96b3667b73ef48eb2
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-11-16 13:49:19 +0000

    Updating the spacing on text fields.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@733 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/template.php
+++ docroot/sites/default/themes/sitetheme/template.php
@@ -447,3 +447,36 @@ function sitetheme_user_profile_form($form) {
   
   return drupal_render($form);
 }
+
+function sitetheme_textfield($element) {
+
+  // If the size is 60 reduce it to 50.
+  if (isset($element['#size']) && $element['#size'] == 60) {
+    $element['#size'] = 50;
+  }
+
+  $size = empty($element['#size']) ? '' : ' size="'. $element['#size'] .'"';
+  $maxlength = empty($element['#maxlength']) ? '' : ' maxlength="'. $element['#maxlength'] .'"';
+  $class = array('form-text');
+  $extra = '';
+  $output = '';
+
+  if ($element['#autocomplete_path'] && menu_valid_path(array('link_path' => $element['#autocomplete_path']))) {
+    drupal_add_js('misc/autocomplete.js');
+    $class[] = 'form-autocomplete';
+    $extra =  '<input class="autocomplete" type="hidden" id="'. $element['#id'] .'-autocomplete" value="'. check_url(url($element['#autocomplete_path'], array('absolute' => TRUE))) .'" disabled="disabled" />';
+  }
+  _form_set_class($element, $class);
+
+  if (isset($element['#field_prefix'])) {
+    $output .= '<span class="field-prefix">'. $element['#field_prefix'] .'</span> ';
+  }
+
+  $output .= '<input type="text"'. $maxlength .' name="'. $element['#name'] .'" id="'. $element['#id'] .'"'. $size .' value="'. check_plain($element['#value']) .'"'. drupal_attributes($element['#attributes']) .' />';
+
+  if (isset($element['#field_suffix'])) {
+    $output .= ' <span class="field-suffix">'. $element['#field_suffix'] .'</span>';
+  }
+
+  return theme('form_element', $element, $output) . $extra;
+}
