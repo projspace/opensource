7bf8564bd164dc62309bc5f362f3e9d1e4c59312
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-11-17 14:20:35 +0000

    Setting textareas to 50 cols wide.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@790 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/template.php
+++ docroot/sites/default/themes/sitetheme/template.php
@@ -502,3 +502,30 @@ function sitetheme_password($element) {
   $output = '<input type="password" name="'. $element['#name'] .'" id="'. $element['#id'] .'" '. $maxlength . $size . drupal_attributes($element['#attributes']) .' />';
   return theme('form_element', $element, $output);
 }
+
+function sitetheme_textarea($element) {
+  // If the cols is 60 reduce it to 50.
+  if (isset($element['#cols']) && $element['#cols'] == 60) {
+    $element['#cols'] = 50;
+  }
+
+  $class = array('form-textarea');
+
+  // Add teaser behavior (must come before resizable)
+  if (!empty($element['#teaser'])) {
+    drupal_add_js('misc/teaser.js');
+    // Note: arrays are merged in drupal_get_js().
+    drupal_add_js(array('teaserCheckbox' => array($element['#id'] => $element['#teaser_checkbox'])), 'setting');
+    drupal_add_js(array('teaser' => array($element['#id'] => $element['#teaser'])), 'setting');
+    $class[] = 'teaser';
+  }
+
+  // Add resizable behavior
+  if ($element['#resizable'] !== FALSE) {
+    drupal_add_js('misc/textarea.js');
+    $class[] = 'resizable';
+  }
+
+  _form_set_class($element, $class);
+  return theme('form_element', $element, '<textarea cols="'. $element['#cols'] .'" rows="'. $element['#rows'] .'" name="'. $element['#name'] .'" id="'. $element['#id'] .'" '. drupal_attributes($element['#attributes']) .'>'. check_plain($element['#value']) .'</textarea>');
+}
