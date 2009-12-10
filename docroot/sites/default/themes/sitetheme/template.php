8802d8170bcce7344228efc05e8ceced58dd5a8b
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-12-10 23:12:15 +0000

    Updating the share chicklit
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@862 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/template.php
+++ docroot/sites/default/themes/sitetheme/template.php
@@ -147,7 +147,11 @@ function sitetheme_preprocess_node(&$vars) {
 
     $vars['content'] = '<div class="node-main-image">'.theme('imagecache', 'image-full-size', sitetheme_get_imceimage_filepath($vars['node']->field_image[0]['imceimage_path']), $vars['node']->field_image[0]['imceimage_alt']) .'</div>'. $vars['content'];
   }
-  $vars['add_this'] = _addthis_create_button($vars['node'], !$vars['page']);
+
+  $vars['add_this'] = '';
+  if ($vars['page'] && user_access('view addthis')) {
+    $vars['add_this'] = _addthis_create_button($vars['node'], !$vars['page']);
+  }
 }
 
 function sitetheme_preprocess_views_view_fields__channel_description__block_1(&$vars) {
