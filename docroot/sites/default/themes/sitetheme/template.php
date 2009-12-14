759957e0cd22dc1a6e0586ec236022705c4e57bc
palantir <palantir@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2009-12-14 17:27:44 +0000

    Updating for CC.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@869 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- docroot/sites/default/themes/sitetheme/template.php
+++ docroot/sites/default/themes/sitetheme/template.php
@@ -154,7 +154,9 @@ function sitetheme_preprocess_node(&$vars) {
   }
   
   if ($vars['node']->type == 'post' && $vars['page']) {
-    //
+    print '<pre>';
+print_r($vars);
+print '</pre>';
   }
 }
 
