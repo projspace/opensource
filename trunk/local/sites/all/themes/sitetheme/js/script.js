b0efa2af67c1638be0d482df1abaa8b320b43210
acquia_support <acquia_support@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2010-02-21 00:00:51 +0000

    Rolling back trunk. Syntax issue.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@1077 ba871ea3-d002-4683-b738-bf6965e0a2a8

---
+++ trunk/local/sites/all/themes/sitetheme/js/script.js
@@ -0,0 +1,9 @@
+(function($){
+  Drupal.behaviors.sitetheme = function(context) {
+    $('#search-theme-form .form-text').val(Drupal.t('Search opensource.com')).focus(function() {
+      $(this).val(Drupal.t(''));
+    }).blur(function() {
+      $(this).val(Drupal.t('Search opensource.com'));
+    });
+  }
+})(jQuery)
