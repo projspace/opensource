fa15491a82dba9327972c844837bea420f3a88c7
acquia_support <acquia_support@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2010-11-12 23:04:42 +0000

    2010-11-12 @acquia-PB ticket #15066-8370
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@1219 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- local/sites/all/themes/sitetheme/template.php
+++ local/sites/all/themes/sitetheme/template.php
@@ -133,6 +133,7 @@ function sitetheme_preprocess_page(&$vars, $hook) {
   $vars['closure'] .= '<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=opensourceway"></script>';
 }
 
+
 /**
  * Override or insert variables into the node templates.
  *
@@ -142,23 +143,21 @@ function sitetheme_preprocess_page(&$vars, $hook) {
  *   The name of the template being rendered ("node" in this case.)
  */
 function sitetheme_preprocess_node(&$vars) {
-  
   $vars['social_bottons'] = '';
   if ($vars['page'] && !in_array($vars['node']->type, array("webform"))) {
     $url = url("node/" . $vars['node']->nid, array("query" => "sc_cid=70160000000IDmjAAG", "absolute" => TRUE));
     // shorten
-    if (module_exists("shorten")) {
-      $url = shorten_url($url);
-    }
+    //if (module_exists("shorten")) {
+    //  $url = shorten_url($url);
+    //}
     // urlencode
     $url_enc = urlencode($url);
     $title = urlencode($vars["node"]->title);
-
+    
     // add share link
     $vars['social_bottons'] = '<a class="addthis_button_tweet" addthis:url="' . $url . '" addthis:title="' . $vars["node"]->title . '"></a>';
-    $vars['social_bottons'] .= ' <a href="javascript:(function(){var%20d=document,w=window,e=w.getSelection,k=d.getSelection,x=d.selection,s=(e?e():(k)?k():(x?x.createRange().text:0)),f=\'http://identi.ca//index.php?action=bookmarklet\',l=d.location,e=encodeURIComponent,g=f+\'&status_textarea=%E2%80%9C\'+((e(s))?e(s):e(document.title))+\'%E2%80%9D%20%E2%80%94%20\'+\'' . $url_enc . '\';function%20a(){if(!w.open(g,\'t\',\'toolbar=0,resizable=0,scrollbars=1,status=1,width=450,height=200\')){l.href=g;}}a();})()"><img src="http://www.nuxified.org/images/identica.png" style="border:
-1px solid #CCCCCC; padding:2px;" /></a>';
-    $vars['social_bottons'] .= ' <a href="http://reddit.com/submit?url=' . $url_enc . '" onclick="window.location = \'http://reddit.com/submit?url=' . $url . '\'; return false"><img src="http://reddit.com/static/spreddit7.gif" alt="submit to reddit" border="0" /></a>';
+    $vars['social_bottons'] .= ' <a href="javascript:(function(){var%20d=document,w=window,e=w.getSelection,k=d.getSelection,x=d.selection,s=(e?e():(k)?k():(x?x.createRange().text:0)),f=\'http://identi.ca//index.php?action=bookmarklet\',l=d.location,e=encodeURIComponent,g=f+\'&status_textarea=%E2%80%9C\'+((e(s))?e(s):e(document.title))+\'%E2%80%9D%20%E2%80%94%20\'+\'' . $url_enc . '\';function%20a(){if(!w.open(g,\'t\',\'toolbar=0,resizable=0,scrollbars=1,status=1,width=450,height=200\')){l.href=g;}}a();})()"><img src="http://www.nuxified.org/images/identica.png" style="border:1px solid #CCCCCC; padding:2px;" /></a>';
+    $vars['social_bottons'] .= ' <a href="http://reddit.com/submit?url=' . $url_enc . '&title=' . $title . '" onclick="window.location = \'http://reddit.com/submit?url=' . $url_enc . '&title=' . $title . '\'; return false"><img src="http://reddit.com/static/spreddit7.gif" alt="submit to reddit" border="0" /></a>';
     $vars['social_bottons'] .= ' <script src="http://www.stumbleupon.com/hostedbadge.php?s=1&r=' . $url . '"></script>';
     $vars['social_bottons'] .= ' <a class="addthis_button_facebook_like" fb:like:layout="button_count" addthis:url="' . $url . '" addthis:title="' . $vars["node"]->title . '"></a>';
 
@@ -247,16 +246,15 @@ function sitetheme_preprocess_comment(&$vars, $hook) {
   // Role 31 is the comment gardner.
   if (isset($user->roles[31])) {
     sitetheme_remove_link('report to Mollom', $vars);
-
     // The sitetheme_remove_link function is stripping out the ul tag in this case.
     // Adding it back.
     $vars['links'] = '<ul class="links">'. $vars['links'];
   }
-
   $username = theme('username', $vars['comment']);
   $vars['submitted'] = t('by !author on !date', array('!author' => $username, '!date' => format_date($vars['comment']->timestamp, 'custom', 'j M Y')));
 }
 // */
+
 
 /**
  * Override or insert variables into the block templates.
@@ -276,6 +274,7 @@ function sitetheme_preprocess_block(&$vars, $hook) {
   }
 }
 
+
 /**
  * Process variables for comment-wrapper.tpl.php.
  *
@@ -283,9 +282,6 @@ function sitetheme_preprocess_block(&$vars, $hook) {
  * @see theme_comment_wrapper()
  */
 function sitetheme_preprocess_comment_wrapper(&$variables) {
-
-  //print_r($variables['node']);
-
   // Provide contextual information.
   $variables['display_mode']  = _comment_get_display_setting('mode', $variables['node']);
   $variables['display_order'] = _comment_get_display_setting('sort', $variables['node']);
@@ -293,9 +289,10 @@ function sitetheme_preprocess_comment_wrapper(&$variables) {
   $variables['template_files'][] = 'comment-wrapper-'. $variables['node']->type;
 
   if ($variables['node']->comment_count > 0) {
-      $variables['comment_count'] .= '<span class="comment-count">'. format_plural($variables['node']->comment_count, '1 Comment', '@count Comments', array('@count' => $variables['node']->comment_count)) .'</span>';
+    $variables['comment_count'] .= '<span class="comment-count">'. format_plural($variables['node']->comment_count, '1 Comment', '@count Comments', array('@count' => $variables['node']->comment_count)) .'</span>';
   }
 }
+
 
 function sitetheme_preprocess_views_view_field__homepage__page_1__teaser(&$vars) {
   if (module_exists('ed_readmore')) {
@@ -313,6 +310,7 @@ function sitetheme_preprocess_views_view_field__homepage__page_1__teaser(&$vars)
   }
 }
 
+
 function sitetheme_preprocess_views_view_fields__channel_description__block_1(&$vars) {
   $vars['about_url'] = '';
   if (arg(0) == 'node' && is_numeric(arg(1))) {
@@ -322,10 +320,12 @@ function sitetheme_preprocess_views_view_fields__channel_description__block_1(&$
   }
 }
 
+
 function sitetheme_preprocess_views_view_field__group_nid(&$vars) {
   // $vars['output'] = t('Open ') . $vars['output'] . ' &raquo; ';
   $vars['output'] = $vars['output'] . ' &raquo; ';
 }
+
 
 function sitetheme_preprocess_views_view_field__value(&$vars) {
   if (isset($vars['row']->users_uid)) {
@@ -336,6 +336,7 @@ function sitetheme_preprocess_views_view_field__value(&$vars) {
   }
 }
 
+
 function sitetheme_preprocess_views_view_field__featured_comment__value(&$vars) {
   if (isset($vars['row']->users_comments_uid)) {
     $account = new stdClass();
@@ -344,6 +345,7 @@ function sitetheme_preprocess_views_view_field__featured_comment__value(&$vars) 
     $vars['output'] = theme('username', $account);
   }
 }
+
 
 function sitetheme_preprocess_views_view__channel_reading__block(&$vars) {
   // Being on a path like node/42 means that 42 is the group
@@ -371,12 +373,15 @@ function sitetheme_preprocess_views_view__channel_reading__block(&$vars) {
   }
 }
 
+
 function sitetheme_preprocess_views_view_field__channel_reading__page__title(&$vars) {
 }
+
 
 function sitetheme_preprocess_views_view_fields__channel_reading__page_1(&$vars) {
   $vars['fields']['title']->content = '<h2 class="title>">' . $vars['fields']['title']->content . '</h2>';
 }
+
 
 function sitetheme_preprocess_views_view_field__channel_reading__comment_count(&$vars) {
   $path = drupal_get_path('theme', 'sitetheme') . '/images/comment.jpg';
@@ -384,6 +389,7 @@ function sitetheme_preprocess_views_view_field__channel_reading__comment_count(&
   $image = theme('image', $path, t('Read comments'), t('Read comments'), array('class' => 'comment-image'));
   $vars['comment_link'] = l($image, 'node/' . $nid, array('html' => TRUE));
 }
+
 
 function sitetheme_preprocess_views_view_field__page__comment_count(&$vars) {
   $nid = $vars['row']->nid;
@@ -396,6 +402,7 @@ function sitetheme_preprocess_views_view_field__page__comment_count(&$vars) {
   }
 }
 
+
 function sitetheme_preprocess_views_view__channel_reading__page_1(&$vars) {
   // This code makes the right primary link have the active class.
   $item = menu_get_item();
@@ -407,6 +414,8 @@ function sitetheme_preprocess_views_view__channel_reading__page_1(&$vars) {
 function sitetheme_preprocess_views_view_field__channel_reading__page__name(&$vars) {
   $vars['output'] .= '<span class="reading-author-feed"><a href="/user/'. $vars['row']->users_uid .'/feed" class="article-author-feed"></a></span>';
 }
+
+
 /**
  * Format the "Submitted by username on date/time" for each node
  *
@@ -419,6 +428,8 @@ function sitetheme_node_submitted($node) {
       '@datetime' => format_date($node->created, 'custom', 'j M Y')
     ));
 }
+
+
 /**
  * Theme a "Submitted by ..." notice.
  *
@@ -434,18 +445,15 @@ function sitetheme_comment_submitted($comment) {
     ));
 }
 
+
 function sitetheme_links($links, $attributes = array('class' => 'links')) {
   $output = '';
-
   if (count($links) > 0) {
     $output = '<ul'. drupal_attributes($attributes) .'>';
-
     $num_links = count($links);
     $i = 1;
-
     foreach ($links as $key => $link) {
       $class = $key;
-
       // Add first, last and active classes to the list of links to help out themers.
       if ($i == 1) {
         $class .= ' first';
@@ -456,7 +464,6 @@ function sitetheme_links($links, $attributes = array('class' => 'links')) {
       if (isset($link['href']) && ($link['href'] == $_GET['q'] || ($link['href'] == '<front>' && drupal_is_front_page()))) {
         $class .= ' active';
       }
-
       if (isset($link['href'])) {
         // add active class for containing <li> and <a> if active-trail is set on the link itself
         if (strpos($link['attributes']['class'], 'active-trail') !== FALSE && strpos($class, 'active') === FALSE) {
@@ -480,17 +487,16 @@ function sitetheme_links($links, $attributes = array('class' => 'links')) {
         }
         $link = '<span'. $span_attributes .'>'. $link['title'] .'</span>';
       }
-
       $i++;
       $output .= '<li'. drupal_attributes(array('class' => $class)) .'>';
       $output .= $link;
       $output .= "</li>\n";
     }
-
     $output .= '</ul>';
   }
   return $output;
 }
+
 
 function sitetheme_box($title, $content, $region = 'main') {
   if ($region == 'main' && $title == t('Post new comment')) {
@@ -499,6 +505,7 @@ function sitetheme_box($title, $content, $region = 'main') {
   $output = '<h2 class="title">'. $title .'</h2><div>'. $content .'</div>';
   return $output;
 }
+
 
 /**
  * Remove a tab
@@ -511,13 +518,13 @@ function sitetheme_box($title, $content, $region = 'main') {
 function sitetheme_remove_tab($label, &$vars) {
   $tabs = explode("\n", $vars['tabs']);
   $vars['tabs'] = '';
-
   foreach($tabs as $tab) {
     if(strpos($tab, '>'. $label .'<') === FALSE) {
       $vars['tabs'] .= $tab . "\n";
     }
   }
 }
+
 
 /**
  * Remove a link
@@ -530,13 +537,13 @@ function sitetheme_remove_tab($label, &$vars) {
 function sitetheme_remove_link($label, &$vars) {
   $tabs = explode("\n", $vars['links']);
   $vars['links'] = '';
-
   foreach($tabs as $tab) {
     if(strpos($tab, '>'. $label .'<') === FALSE) {
       $vars['links'] .= $tab . "\n";
     }
   }
 }
+
 
 /**
  * Theme the fivestar form element by adding necessary css and javascript.
@@ -550,12 +557,11 @@ function sitetheme_fivestar($element) {
       $element['#description'] = '<div class="fivestar-summary">&nbsp;</div>';
     }
   }
-
   $output = theme('form_element', $element, $element['#children']);
   $output .= t('<div class="fivestar-total">(!num_votes votes)</div>', array('!num_votes' => $element['#vote_count']));
-
   return $output;
 }
+
 
 /**
  * Helper function to get the local path for an image when the full path is what we have.
@@ -572,6 +578,7 @@ function sitetheme_get_imceimage_filepath($path) {
   return $subdir .'/'. $filename;
 }
 
+
 /**
  * Overriding the file element to set the default size to 30 wide rather than 60.
  */
@@ -583,29 +590,28 @@ function sitetheme_file($element) {
   return theme('form_element', $element, '<input type="file" name="'. $element['#name'] .'"'. ($element['#attributes'] ? ' '. drupal_attributes($element['#attributes']) : '') .' id="'. $element['#id'] .'" size="'. $element['#size'] ."\" />\n");
 }
 
+
 function sitetheme_user_profile_form($form) {
   // Removing the link to the personal contact form. If you are on your own contact form this takes you to a page
   // you do not have access to.
   if (isset($form['contact']['contact']['#description'])) {
     $form['contact']['contact']['#description'] = t('Allow other users to contact you by e-mail via your personal contact form. Note that while your e-mail address is not made public to other members of the community, privileged users such as site administrators are able to contact you even if you choose not to enable this feature.');
   }
-
   return drupal_render($form);
 }
+
 
 function sitetheme_user_register($form) {
   $form['legal']['legal_accept']['#title'] = t('Accept the !terms to continue.  You are licensing your contribution(s) as CC-BY-SA.', array('!terms' => l('Terms of Use', 'legal')));
   if (isset($form['legal']['conditions']['#title'])) {
     $form['legal']['conditions']['#title'] = t('Terms of Use');
   }
-
   return drupal_render($form);
 }
 
-function sitetheme_textfield($element) {
 
-  // If the size is 60 reduce it to 50. 60 is the standard size and 58 is the
-  // size of OpenID.
+function sitetheme_textfield($element) {
+  // If the size is 60 reduce it to 50. 60 is the standard size and 58 is the size of OpenID.
   if (isset($element['#size']) && ($element['#size'] == 60 || $element['#size'] == 58)) {
     $element['#size'] = 50;
   }
@@ -683,7 +689,6 @@ function sitetheme_textarea($element) {
 function sitetheme_username($object) {
   // Store user info so we only load it once.
   static $account_info = array();
-
   if ($object->uid && $object->name) {
     if (!isset($account_info[$object->uid])) {
       $account = new stdClass();
@@ -692,9 +697,7 @@ function sitetheme_username($object) {
       profile_load_profile($account);
       $account_info[$account->uid] = $account;
     }
-
     $name = $account_info[$object->uid]->profile_display_name;
-
     if (user_access('access user profiles')) {
       $output = l($name, 'user/'. $object->uid, array('attributes' => array('title' => t('View user profile.'))));
     }
@@ -715,7 +718,6 @@ function sitetheme_username($object) {
   else {
     $output = check_plain(variable_get('anonymous', t('Anonymous')));
   }
-
   return $output;
 }
 
@@ -727,15 +729,12 @@ function sitetheme_preprocess_views_view_row_rss__channel_reading(&$vars) {
   $result = $vars['view']->result;
   $id = $vars['id'];
   $node = node_load($result[$id-1]->nid);
-
+  
   // override description
   $description = "<div>" . $node->title . ": " . $node->field_url[0]["url"] . " - " 
     . $node->field_url[0]["title"] . "</div>";
-
-
   $description = '<div>' . $node->title . " - " . $node->field_url[0]["title"] . '</div>';
-
-
+  
   // update rss item
   $vars["title"] = check_plain($node->field_url[0]["title"]);
   $vars["link"] = check_plain($node->field_url[0]["url"]);
