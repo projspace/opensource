e670ebc6e08e2d3ce6df151b519fca23d26b7525
acquia_support <acquia_support@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2011-01-24 00:22:59 +0000

    Updating vud with patch
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@1265 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- local/sites/all/themes/sitetheme/templates/comment.tpl.php
+++ local/sites/all/themes/sitetheme/templates/comment.tpl.php
@@ -63,10 +63,8 @@ global $user;
   </div>
 
   <div class="comment-bottom">
-    <?php if(user_access('use vote up/down on comments')): ?>
-      <?php print $vote_up_down; ?>
-    <?php endif; ?>
-  <?php print $links ?>
+    <?php print $vote_up_down; ?>
+    <?php print $links ?>
   </div>
   
 </div>
