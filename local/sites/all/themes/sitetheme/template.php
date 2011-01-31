b25461a049f6dd90c8ab79a20d85a5a79da8eca4
acquia_support <acquia_support@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2011-01-31 13:02:23 +0000

    Updating userpoints to 6.x-1.x-DEV from January 25, 2011
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@1297 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- local/sites/all/themes/sitetheme/template.php
+++ local/sites/all/themes/sitetheme/template.php
@@ -263,7 +263,9 @@ function sitetheme_preprocess_comment(&$vars, $hook) {
   }
   $username = theme('username', $vars['comment']);
   $vars['submitted'] = t('by !author on !date', array('!author' => $username, '!date' => format_date($vars['comment']->timestamp, 'custom', 'j M Y')));
-  $vars['badges'] = user_badges_for_user(array('uid' => $vars['comment']->uid));
+  
+  // Heavy Modification of Badge Display
+  $vars['role_badge'] = opensource_points_comment_badges_for_user()
 }
 // */
 
