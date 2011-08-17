6e6eb3c7b149106967d7fc7122898f59c7fdf739
acquia_support <acquia_support@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2011-08-17 16:54:32 +0000

    Changing opensource.com image credits
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@1401 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- local/sites/all/themes/sitetheme/template.php
+++ local/sites/all/themes/sitetheme/template.php
@@ -212,7 +212,7 @@ function sitetheme_preprocess_node(&$vars) {
       $caption = '<br /><span class="node-main-image-caption">' . t('Image credits: !caption', array('!caption' => $caption)) . '</span>';
     }
     else {
-      $caption = '';
+      $caption = '<br /><span class="node-main-image-caption">Image by opensource.com</span>';
     }
     if (isset($vars['node']->field_lead_image[0]['view'])) {
       $field_lead_image_view = $vars['node']->field_lead_image[0]['view'];
@@ -221,6 +221,8 @@ function sitetheme_preprocess_node(&$vars) {
         $caption = $vars['node']->field_lead_image[0]['data']['title'];
         if($caption) {
           $caption = '<br /><span class="node-main-image-caption">' . t('Image credits: !caption', array('!caption' => $caption)) . '</span>';
+        } else {
+          $caption = '<br /><span class="node-main-image-caption">Image by opensource.com</span>';
         }
         $vars['content'] = '<div class="node-main-image">'. $field_lead_image  . $caption .'</div>' . $vars['content'];
       }
