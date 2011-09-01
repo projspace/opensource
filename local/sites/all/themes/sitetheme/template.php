5d3df4c2e9ed12693f3e86f511bfa480638867e9
acquia_support <acquia_support@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2011-09-01 20:05:18 +0000

    Adding account for podcast
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@1422 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- local/sites/all/themes/sitetheme/template.php
+++ local/sites/all/themes/sitetheme/template.php
@@ -211,7 +211,7 @@ function sitetheme_preprocess_node(&$vars) {
   if ($vars['node']->type == 'channel') {
     $vars['links'] = '';
   }
-  elseif ($vars['node']->type == 'post' && !$vars['page']) {
+  elseif (($vars['node']->type == 'post' || $vars['node']->type == 'podcast') && !$vars['page']) {
     $vars['links_all'] = '<div class="clearfix"></div><div class="article-visitor-links clear-block">';
     if ($vars['node']->comment_count > 0) {
       $vars['links_all'] .= '<span class="article-comment-count">'. l(format_plural($vars['node']->comment_count, '1 Comment', '@count Comments', array('@count' => $vars['node']->comment_count)), 'node/'. $vars['node']->nid, array('fragment' => 'comments')) .'</span>';
