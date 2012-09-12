5d18d6d87ae1f2705222e2dbf8e860a40422aced
acquia_support <acquia_support@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2012-09-12 21:09:30 +0000

    Adding Omniture code, 15066-47824
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@1585 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- local/sites/all/libraries/omniture/rh_omni_footer.js
+++ local/sites/all/libraries/omniture/rh_omni_footer.js
@@ -31,8 +31,12 @@ if (checkRegEvent() == 1) {
     s.events = "event2";
 }
 
-s.eVar18 = window.location.href;
-s.prop21 = window.location.href;
+
+//  var urlparts= url.split('?');
+var address = split(window.location.href, '?');
+s.eVar18 = address[0];
+s.prop21 = address[0];
+
 
 /* */
 
@@ -111,7 +115,7 @@ function getCookie(name)
     if (begin != 0) return null;
   } else {
     begin += 2;
-    
+
   }
   var end = document.cookie.indexOf(";", begin);
   if (end == -1) {
@@ -120,16 +124,16 @@ function getCookie(name)
   return unescape(dc.substring(begin + prefix.length, end));
 }
 
-function setCookie( name, value, expires, path, domain, secure ) 
+function setCookie( name, value, expires, path, domain, secure )
 {
 // set time, it's in milliseconds
 var today = new Date();
 today.setTime( today.getTime() );
 
 /*
-if the expires variable is set, make the correct 
-expires time, the current script below will set 
-it for x number of days, to make it for hours, 
+if the expires variable is set, make the correct
+expires time, the current script below will set
+it for x number of days, to make it for hours,
 delete * 24, for minutes, delete * 60 * 24
 */
 if ( expires )
@@ -140,12 +144,12 @@ if ( expires )
 var expires_date = new Date( today.getTime() + (expires) );
 
 document.cookie = name + "=" +escape( value ) +
-( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) + 
-( ( path ) ? ";path=" + path : "" ) + 
+( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) +
+( ( path ) ? ";path=" + path : "" ) +
 ( ( domain ) ? ";domain=" + domain : "" ) +
 ( ( secure ) ? ";secure" : "" );
 }
-			
+
 // this deletes the cookie when called
 function deleteCookie( name, path, domain ) {
 if ( getCookie( name ) ) document.cookie = name + "=" +
@@ -153,11 +157,3 @@ if ( getCookie( name ) ) document.cookie = name + "=" +
   ( ( domain ) ? ";domain=" + domain : "" ) +
 ";expires=Thu, 01-Jan-1970 00:00:01 GMT";
 }
-
-
-
-
-
-
-
-
