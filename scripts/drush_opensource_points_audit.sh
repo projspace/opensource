790bdaf92c318c4a6f66d4a2dc47af6569ad4bef
acquia_ahsupport_dwebber <acquia_ahsupport_dwebber@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2012-11-08 17:25:28 +0000

    Acquia Support: added bash script to facilitate running drush osp-ba on the cron.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@1606 ba871ea3-d002-4683-b738-bf6965e0a2a8

---
+++ scripts/drush_opensource_points_audit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+drush='/usr/bin/drush'
+
+# todo: pick these up from the alias definitions rather than duplicating it here
+config_prod='--uri=dev.opensource.com --root=/mnt/www/aradev/docroot'
+config_dev='--uri=www.opensource.com --root=/mnt/www/ara/docroot'
+
+# default to dev
+drush_command="$drush $config_dev"
+if [ -z "$1" ]; then
+  if [ "$1" == 'prod' ]; then
+    drush_command="$drush $config_prod"
+  fi
+fi
+
+#  $items['osp-audit-badges'] = array(
+#    'description' => 'Audit user badges and roles.',
+#    'options' => array(
+#        'show-failures'   => 'Show info about each failure',
+#        'show-debug'      => 'Lots of debug output',
+#        'quiet'           => 'Suppress all output',
+#        'repair'          => 'Repair any problems which are found',
+#        'no-logging'      => 'Do not log to watchdog',
+#      ),  
+#    'arguments' => array(
+#        'limit'   => 'Only process the first X items',
+#      ),  
+#    'aliases' => array('osp-ab'),
+#  ); 
+
+#audit_options='--repair --quiet'   # this is probably how we want the task to run on cron
+audit_options='--limit=50'          # debug just by examining a small number of users
+
+$drush_command osp-ab $audit_options
+
