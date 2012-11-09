4b0d4f0bd0bef1a495358f138f595f3265938a80
acquia_ahsupport_dwebber <acquia_ahsupport_dwebber@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2012-11-09 09:49:52 +0000

    Acquia support: fix silly mistake in bash drush osp-ab script.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@1609 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- scripts/drush_opensource_points_audit.sh
+++ scripts/drush_opensource_points_audit.sh
@@ -2,15 +2,15 @@
 
 # bash wrapper script to run drush osp-audit-badges command
 #   takes an optional argument dev|prod to specify which environment to run the command against
-#   (defaults to dev unless 'prod' is specified
+#   (defaults to dev unless 'prod' is specified)
 audit_options='--repair --quiet'   # this is probably how we want the task to run on cron
 #audit_options='50'                # (limit argument) debug by examining a small number of users (and doing no repair)
 
 drush='/usr/bin/drush'
 
 # todo: pick these up from the alias definitions rather than duplicating it here
-config_prod='--uri=dev.opensource.com --root=/mnt/www/aradev/docroot'
-config_dev='--uri=www.opensource.com --root=/mnt/www/ara/docroot'
+config_dev='--uri=dev.opensource.com --root=/mnt/www/aradev/docroot'
+config_prod='--uri=www.opensource.com --root=/mnt/www/ara/docroot'
 
 # default to dev
 drush_command="$drush $config_dev"
