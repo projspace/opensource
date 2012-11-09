eb2ab4415af0bc108fdf4e40d2c7f17f938e1af0
acquia_ahsupport_dwebber <acquia_ahsupport_dwebber@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2012-11-09 09:37:05 +0000

    Acquia support: tidy up of bash drush osp-ab script.
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@1608 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- scripts/drush_opensource_points_audit.sh
+++ scripts/drush_opensource_points_audit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# bash wrapper script to run drush osp-audit-badges command
+#   takes an optional argument dev|prod to specify which environment to run the command against
+#   (defaults to dev unless 'prod' is specified
+audit_options='--repair --quiet'   # this is probably how we want the task to run on cron
+#audit_options='50'                # (limit argument) debug by examining a small number of users (and doing no repair)
+
 drush='/usr/bin/drush'
 
 # todo: pick these up from the alias definitions rather than duplicating it here
@@ -13,24 +19,6 @@ if [ -z "$1" ]; then
     drush_command="$drush $config_prod"
   fi
 fi
-
-#  $items['osp-audit-badges'] = array(
-#    'description' => 'Audit user badges and roles.',
-#    'options' => array(
-#        'show-failures'   => 'Show info about each failure',
-#        'show-debug'      => 'Lots of debug output',
-#        'quiet'           => 'Suppress all output',
-#        'repair'          => 'Repair any problems which are found',
-#        'no-logging'      => 'Do not log to watchdog',
-#      ),  
-#    'arguments' => array(
-#        'limit'   => 'Only process the first X items',
-#      ),  
-#    'aliases' => array('osp-ab'),
-#  ); 
-
-audit_options='--repair --quiet'   # this is probably how we want the task to run on cron
-#audit_options='50'                  # (limit argument) debug just by examining a small number of users
 
 $drush_command osp-ab $audit_options
 
