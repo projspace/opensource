8ad9465da87ffca4ec7de6bb9864f49f1b76bf11
acquia_support <acquia_support@ba871ea3-d002-4683-b738-bf6965e0a2a8>
2012-11-26 14:54:32 +0000

    Acquia ticket 15066-53834: Merge branch acquia-15066-53834 into trunk
    git-svn-id: https://svn-1.prod.hosting.acquia.com/opensource/trunk@1614 ba871ea3-d002-4683-b738-bf6965e0a2a8

--- local/sites/all/themes/sitetheme/templates/views-view-field--articles--page-1--teaser.tpl.php
+++ local/sites/all/themes/sitetheme/templates/views-view-field--articles--page-1--teaser.tpl.php
@@ -1,5 +1,4 @@
 <?php
-// $Id: views-view-field.tpl.php,v 1.1 2008/05/16 22:22:32 merlinofchaos Exp $
  /**
   * This template is used to print a single field in a view. It is not
   * actually used in default Views, as this is registered as a theme
@@ -20,4 +19,4 @@
   * the view is modified.
   */
 ?>
-<?php print $links_all; ?>
+<?php print $output; ?>
