022037141d73154c3acf570c42730981fee2d48e
Ankur Gupta <agupta@axelerant.com>
2014-01-21 14:30:54 +0530

    checking the vendor information

--- docroot/cron.php
+++ docroot/cron.php
@@ -1,1 +1,10 @@
-../vendor/acquia/cron.php
+<?php
+
+/**
+ * @file
+ * Handles incoming requests to fire off regularly-scheduled tasks (cron jobs).
+ */
+
+include_once './includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+drupal_cron_run();
