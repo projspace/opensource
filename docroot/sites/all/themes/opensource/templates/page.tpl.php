3819820e3886e492ca19d689699ef29523e23c96
Bassam Ismail <skippednote@gmail.com>
2014-01-24 20:30:49 +0530

    Add header banner wrapper

--- docroot/sites/all/themes/opensource/templates/page.tpl.php
+++ docroot/sites/all/themes/opensource/templates/page.tpl.php
@@ -18,37 +18,39 @@
 
     <?php print render($page['navigation_top']); ?>
 
-    <?php if ($site_name || $site_slogan): ?>
-      <div class="header__name-and-slogan" id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 class="header__site-name" id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
+    <div class="header-banner">
+        <?php if ($site_name || $site_slogan): ?>
+          <div class="header__name-and-slogan" id="name-and-slogan">
+            <?php if ($site_name): ?>
+              <h1 class="header__site-name" id="site-name">
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+              </h1>
+            <?php endif; ?>
+
+            <?php if ($site_slogan): ?>
+              <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
+            <?php endif; ?>
+          </div>
         <?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php if ($secondary_menu): ?>
+          <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
+            <?php print theme('links__system_secondary_menu', array(
+              'links' => $secondary_menu,
+              'attributes' => array(
+                'class' => array('links', 'inline', 'clearfix'),
+              ),
+              'heading' => array(
+                'text' => $secondary_menu_heading,
+                'level' => 'h2',
+                'class' => array('element-invisible'),
+              ),
+            )); ?>
+          </nav>
         <?php endif; ?>
-      </div>
-    <?php endif; ?>
 
-    <?php if ($secondary_menu): ?>
-      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
+        <?php print render($page['header']); ?>
+    </div>
 
   </header>
 
