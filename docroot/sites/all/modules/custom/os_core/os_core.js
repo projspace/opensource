dcaef6ff47fe065ea7f4ec207dbd524436234b76
Mohit Aghera <mohit@axelerant.com>
2014-02-25 15:23:29 +0530

    Fixed the javascript bug and and coding standard issue.

--- docroot/sites/all/modules/custom/os_core/os_core.js
+++ docroot/sites/all/modules/custom/os_core/os_core.js
@@ -1,11 +1,13 @@
-jQuery(document).ready(function() {
+(function ($) {
+  $(function () {
     tinyMCE.init({
       mode: "none",
       theme: "advanced",
       plugins : "autolink,lists,spellchecker,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
       theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,bullist,numlist,blockquote,|,justifyleft,justifycenter,justifyright,justifyfull,|,link,unlink,pagebreak,|,fullscreen,spellchecker,media,",
       skin : "cirkuit",
-      width: "100%",
+      width: "100%"
     });
-    tinymce.execCommand("mceAddControl", false, "edit-field-default-license-und-other");
-});
+    tinyMCE.execCommand("mceAddControl", false, "edit-field-default-license-und-other");
+  })
+})(jQuery);
